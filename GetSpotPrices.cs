3c98fa8dfa84ba879c9e7963620efda53e2c66c5
Mattias Sämskar <mattias.samskar@gmail.com>
2022-10-22 11:06:47 +0200

    Fix error when no pricepoints exist

--- GetSpotPrices.cs
+++ GetSpotPrices.cs
@@ -25,14 +25,15 @@ namespace SpotPrices
         ILogger log)
     {
       log.LogInformation("GetPrices function triggered.");
-
       var clock = SystemClock.Instance.InZone(DateTimeZoneProviders.Tzdb["Europe/Stockholm"]);
+      log.LogInformation("Clock is " + clock.GetCurrentLocalDateTime());
       var today = clock.GetCurrentDate();
       var tomorrow = today.PlusDays(1);
+      log.LogInformation("Today is " + today + " and tomorrow is " + tomorrow);
 
-      var exchangeRate = await GetExchangeRate(today);
-      var todayPricePoints = await GetPricePoints(today);
-      var tomorrowPricePoints = await GetPricePoints(tomorrow);
+      var exchangeRate = await GetExchangeRate(today, log);
+      var todayPricePoints = await GetPricePoints(today, log);
+      var tomorrowPricePoints = await GetPricePoints(tomorrow, log);
       var todayPrices = ConvertToSekPerKwh(todayPricePoints, exchangeRate);
       var tomorrowPrices = ConvertToSekPerKwh(tomorrowPricePoints, exchangeRate);
 
@@ -56,47 +57,65 @@ namespace SpotPrices
       return pricePoints.Select(pricePoint => new PricePoint { Amount = Math.Round(pricePoint.Amount * exchangeRate) / 10 });
     }
 
-    static async Task<double> GetExchangeRate(LocalDate localDate)
+    static async Task<double> GetExchangeRate(LocalDate localDate, ILogger log)
     {
-      var date = localDate.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
-
-      var url = $"https://api.apilayer.com/exchangerates_data/v1/{date}?base=EUR&symbols=SEK";
-      var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, url);
-      httpRequestMessage.Headers.Add("apikey", System.Environment.GetEnvironmentVariable("ApiLayer_ApiKey"));
-
-      var response = await new HttpClient().SendAsync(httpRequestMessage);
-
-      if (!response.IsSuccessStatusCode)
+      var defaultExchangeRate = System.Environment.GetEnvironmentVariable("DefaultExchangeRate");
+      try
       {
-        var defaultExchangeRate = System.Environment.GetEnvironmentVariable("DefaultExchangeRate");
+        var date = localDate.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+        var url = $"https://api.apilayer.com/exchangerates_data/v1/{date}?base=EUR&symbols=SEK";
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+        httpRequestMessage.Headers.Add("apikey", System.Environment.GetEnvironmentVariable("ApiLayer_ApiKey"));
+
+        var response = await new HttpClient().SendAsync(httpRequestMessage);
+
+        if (!response.IsSuccessStatusCode)
+        {
+          log.LogError("Could not fetch exhange rate");
+          return double.Parse(defaultExchangeRate);
+        }
+
+        var result = await response.Content.ReadAsStringAsync();
+        var jObject = JObject.Parse(result);
+        return jObject["rates"]["SEK"].Value<double>();
+      }
+      catch
+      {
+        log.LogError("Could not parse exchange rate");
         return double.Parse(defaultExchangeRate);
       }
-
-      var result = await response.Content.ReadAsStringAsync();
-      var jObject = JObject.Parse(result);
-      return jObject["rates"]["SEK"].Value<double>();
     }
 
-    static async Task<IEnumerable<PricePoint>> GetPricePoints(LocalDate localDate)
+    static async Task<IEnumerable<PricePoint>> GetPricePoints(LocalDate localDate, ILogger log)
     {
-      var securityToken = System.Environment.GetEnvironmentVariable("Entsoe_SecurityToken");
-      var period = localDate.PlusDays(-1).ToString("yyyyMMdd2300", CultureInfo.InvariantCulture);
-      var url = $"https://web-api.tp.entsoe.eu/api?documentType=A44&in_Domain=10Y1001A1001A46L&out_Domain=10Y1001A1001A46L&periodStart={period}&periodEnd={period}&securityToken={securityToken}";
+      try
+      {
+        var securityToken = System.Environment.GetEnvironmentVariable("Entsoe_SecurityToken");
+        var period = localDate.PlusDays(-1).ToString("yyyyMMdd2300", CultureInfo.InvariantCulture);
+        var url = $"https://web-api.tp.entsoe.eu/api?documentType=A44&in_Domain=10Y1001A1001A46L&out_Domain=10Y1001A1001A46L&periodStart={period}&periodEnd={period}&securityToken={securityToken}";
 
-      HttpResponseMessage response = await new HttpClient().GetAsync(url);
+        HttpResponseMessage response = await new HttpClient().GetAsync(url);
 
-      if (!response.IsSuccessStatusCode) return new List<PricePoint>();
+        if (!response.IsSuccessStatusCode)
+        {
+          log.LogError("Could not fetch price points");
+          return new List<PricePoint>();
+        }
+        var result = await response.Content.ReadAsStringAsync();
 
-      var result = await response.Content.ReadAsStringAsync();
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(result);
+        var jObject = JObject.Parse(JsonConvert.SerializeXmlNode(doc));
+        IList<JToken> results = jObject["Publication_MarketDocument"]["TimeSeries"]["Period"]["Point"].Children().ToList();
 
-      XmlDocument doc = new XmlDocument();
-      doc.LoadXml(result);
-
-      var jObject = JObject.Parse(JsonConvert.SerializeXmlNode(doc));
-
-      IList<JToken> results = jObject["Publication_MarketDocument"]["TimeSeries"]["Period"]["Point"].Children().ToList();
-
-      return results.Select(result => result.ToObject<PricePoint>());
+        return results.Select(result => result.ToObject<PricePoint>());
+      }
+      catch
+      {
+        log.LogError("Error while parsing price points");
+        return new List<PricePoint>();
+      }
     }
   }
 }
